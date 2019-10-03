c1612f39a1ce6cedb881bf582eb6ddea7132249a
course-fpuna <juanjarolin94@fpuna.edu.py>
2019-10-03 08:12:58 -0400

    agrego frontend

--- Proyecto/backend/src/index.js
+++ Proyecto/backend/src/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
 const SocketIo = require('socket.io');
+const cors = require('cors');
+const axios = require('axios');
 
 require('dotenv').config();
 
@@ -17,6 +19,7 @@ app.use(express.static(path.join(__dirname, 'public'))); //public, accedida por 
 //middlewares
 app.use(morgan('dev'));
 app.use(express.json());
+app.use(cors());
 
 //routes
 app.use('/api/productos', require('./routes/productos.routes'));
@@ -29,6 +32,13 @@ const server = app.listen(app.get('port'), () => {
 
 const io = SocketIo(server);
 
-io.on('connect', () => {
-   console.log('Alguien conectándose');
+io.on('connection', (socket) => {
+   socket.on('fetchProductos', async () => {
+      try {
+         const res = await axios.get('http://localhost:4000/api/productos');
+         await socket.emit('Productos', res.data)
+      } catch (error) {
+         console.log('Error')
+      }
+   });
 });
---
+++ Proyecto/frontend/src/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min'
+import 'react-notifications/lib/notifications.css'
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import io from 'socket.io-client'
+import axios from 'axios'
+import Navbar from './components/Navegacion'
+import Productos from './components/Productos'
+
+
+const socket = io.connect('http://localhost:4000')
+axios.defaults.baseURL = "http://localhost:4000"
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route exact path="/productos" component={() => <Productos axios={axios} socket={socket}/>}/>
+        </Switch>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
